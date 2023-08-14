21178ee1c9d5b53b31c0ff2c1914b76f50b5e3c9
DmitryJava <dmitry63java@gmail.com>
2023-08-14 19:05:52 +0400

    added day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,25 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+        String string = "";
+        for(int i = 0; i <= 20000; i++) {
+            string += i + " ";
+        }
+        System.out.println(string);
+        long stopTime = System.currentTimeMillis();
+        System.out.println();
+        System.out.printf("Длительность работы, в мс.: %s\n\n", stopTime - startTime);
+
+
+        startTime = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString());
+        stopTime = System.currentTimeMillis();
+        System.out.println();
+        System.out.printf("Длительность работы, в мс.: %s", stopTime - startTime);
     }
 }
